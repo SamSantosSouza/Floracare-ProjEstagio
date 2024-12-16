275ada8cc0d5654d41d27c9fa1e0db6eac46528f
SamSantosSouza <santossamuell1235@gmail.com>
2024-12-15 21:11:07 -0300

    criando o componente header e adicionando um path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "",
+        component: MainComponent
+        }
+    
+];
