60ca303a4c79ed5de3ffd607006bb3d4410e7519
SamSantosSouza <santossamuell1235@gmail.com>
2024-12-22 20:45:57 -0300

    feat: criando e estilizando as outras seções da home

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { NgOptimizedImage } from '@angular/common';
+import { MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage],
+  imports: [HeaderComponent, NgOptimizedImage, MatButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
