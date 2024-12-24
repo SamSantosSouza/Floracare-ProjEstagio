49bd1e50624029bfec6cbe52cc0b59eec5c22f9d
SamSantosSouza <santossamuell1235@gmail.com>
2024-12-24 11:19:14 -0300

    feat: adicionando a footer na pagina principal

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { NgOptimizedImage } from '@angular/common';
 import { MatButtonModule} from '@angular/material/button';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, MatButtonModule],
+  imports: [HeaderComponent, NgOptimizedImage, MatButtonModule, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
