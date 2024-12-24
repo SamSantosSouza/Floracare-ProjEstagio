4453b3701420b53b6c4f29d5ca5d4fed68d976eb
SamSantosSouza <santossamuell1235@gmail.com>
2024-12-24 11:16:36 -0300

    feat: fixando a header na pagina

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from'@angular/material/button';
+import {MatIconModule} from'@angular/material/icon';
+import { NgOptimizedImage } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,MatButtonModule,MatIconModule,NgOptimizedImage
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
