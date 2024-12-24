2142eeb98730aef7a18ca5ef2035a08cdc7e8e10
SamSantosSouza <santossamuell1235@gmail.com>
2024-12-24 11:17:57 -0300

    feat: criando o botão de submit que vai ser utilizado no formulario de contato do footer

---
+++ src/app/components/button-primary/button-primary.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'btn-primary',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-primary.component.html',
+  styleUrl: './button-primary.component.scss'
+})
+export class ButtonPrimaryComponent {
+  @Input ("btn-text") btnText: string = ""
+  @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
+  @Output("submit") onSubmit = new EventEmitter();
+
+  submit(){
+    this.onSubmit.emit();
+  }
+}
