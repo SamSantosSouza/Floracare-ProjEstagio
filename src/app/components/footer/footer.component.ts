8d99dd40bbc461232259a0789ba375fb919f5b65
SamSantosSouza <santossamuell1235@gmail.com>
2024-12-25 12:17:52 -0300

    feat: configurando o envio do formulário de contato para o backend

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,18 +2,23 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ButtonPrimaryComponent } from '../button-primary/button-primary.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { response } from 'express';
+import { error } from 'console';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatToolbarModule, ButtonPrimaryComponent, ReactiveFormsModule],
+  imports: [MatToolbarModule, ButtonPrimaryComponent, ReactiveFormsModule, CommonModule,  HttpClientModule, ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
   footer: FormGroup;
+  loading = false;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.footer = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email])
@@ -21,6 +26,27 @@ export class FooterComponent {
   }
 
   onSubmit(){
-    console.log(this.footer.value)
+    if(this.footer.value) {
+      this.loading = true;
+
+
+    this.http.post('http://localhost:3000/contato', this.footer.value).subscribe(
+    (response) => {
+      console.log( 'formulário enviado com sucesso!', response);
+      this.loading = false
+    },
+    (error) => {
+      console.error('Erro ao enviar o formulário', error);
+      this.loading = false
+    }
+    )
+
+   
+  } else {
+    this.footer.markAllAsTouched()
+    console.log("formulário inválido");
   }
+}
+
+  currentYear: number = new Date().getFullYear();
 }
