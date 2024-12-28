6740ac54d335fcfdd898784a15a730628df1f782
SamSantosSouza <santossamuell1235@gmail.com>
2024-12-28 19:40:33 -0300

    fix: corrigindo as rotas das páginas

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,14 +3,24 @@ import { HeaderComponent } from "../header/header.component";
 import { NgOptimizedImage } from '@angular/common';
 import { MatButtonModule} from '@angular/material/button';
 import { FooterComponent } from "../footer/footer.component";
+import { Router } from '@angular/router';
+import { ContComponent } from '../cont/cont.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, MatButtonModule, FooterComponent],
+  imports: [HeaderComponent, NgOptimizedImage, MatButtonModule, FooterComponent, ContComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-
+   constructor(private router : Router) {}
+  
+    goToAboutUs() {
+      this.router.navigate(['/aboutus']);
+    }
+  
+    goToMain() {
+      this.router.navigate([""]);
+    }
 }
