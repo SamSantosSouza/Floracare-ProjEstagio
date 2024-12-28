b84a98dd5d81e6a32236959bdfc3dee10a88a121
SamSantosSouza <santossamuell1235@gmail.com>
2024-12-28 20:03:06 -0300

    fix: corrgindo o caminho do servidor do formulário de contato

--- backend/servidor-contact/server-contact.js
+++ backend/servidor-contact/server-contact.js
@@ -8,11 +8,11 @@ const PORT = 4000;
 app.use(cors())
 app.use(bodyParser.json())
 
-app.post('/contato', (req, res) => {
+app.post('/contact', (req, res) => {
     console.log('dados recebidos:', req.body);
     res.status(200).json({message: 'Resposta enviado com sucesso!'});
 });
 
 app.listen(PORT, () => {
-    console.log('Servidor rodando em http://localhost:${PORT}');
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
